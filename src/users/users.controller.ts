ef7b6895df4559a9b0a415a5c7a3d95875fe15c7
julio <juliofariadev@gmail.com>
2024-12-15 13:51:11 -0300

    Router decorators and Params, Req, Query and Body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+  Headers,
+  Ip,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id')
+  public getUsers(@Param('id') id: any, @Query() query: any) {
+    console.log(id);
+    console.log(query);
+    return 'You sent a GET request to users endpoint';
+  }
+
+  @Post()
+  public createUsers(
+    @Body('email') email: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(ip);
+    console.log(email);
+    console.log(headers);
+    return 'You sent a POST request to users endpoint';
+  }
+}
