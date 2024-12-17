13af0cdaf51d2c5dad3908c67acca9cd7021b63f
julio <juliofariadev@gmail.com>
2024-12-17 20:27:53 -0300

    practice inter modular dependency

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../../users/providers/user.service';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly usersService: UsersService) {}
+
   public findAll(userId: string) {
-    console.log(userId);
+    const user = this.usersService.findOneById(userId);
+    return [
+      {
+        user: user,
+        title: 'Teste title',
+        content: 'Test Content',
+      },
+      {
+        user: user,
+        title: 'Teste title 2',
+        content: 'Test Content 2',
+      },
+    ];
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,5 +5,6 @@ import { UsersService } from './providers/user.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
