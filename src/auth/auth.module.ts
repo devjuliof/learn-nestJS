0bab48223a346c76de34d04cf918bcb541253110
julio <juliofariadev@gmail.com>
2025-01-13 19:29:32 -0300

    feat: add Jwt config module to imports array

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,9 @@ import { BcryptProvider } from './providers/bcrypt.provider';
 import { HashingProvider } from './providers/hashing.provider';
 import { UsersModule } from 'src/users/users.module';
 import { SignInProvider } from './providers/signin.provider';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
@@ -17,7 +20,11 @@ import { SignInProvider } from './providers/signin.provider';
       useClass: BcryptProvider,
     },
   ],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
